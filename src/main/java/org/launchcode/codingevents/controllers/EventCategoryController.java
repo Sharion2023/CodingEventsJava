2db1c23ab025b202dd4eea3ed6f313905a460c9f
Sharion2023 <Sharion06@gmail.com>
2023-10-27 20:54:17 -0500

    Added event category creation

--- src/main/java/org/launchcode/codingevents/controllers/EventCategoryController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventCategoryController.java
@@ -5,7 +5,7 @@ import org.launchcode.codingevents.data.EventCategoryRespository;
 
 import org.launchcode.codingevents.models.EventCategory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.Banner;import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
@@ -25,14 +25,14 @@ public class EventCategoryController {
         return "eventCategories/index";
     }
 
-    @GetMapping
+    @GetMapping("/create")
     public String renderCreateEventCategoryForm(Model model){
     model.addAttribute("title", "Event Category");
     model.addAttribute(new EventCategory());
       return "eventCategories/create";
     }
 
-    @PostMapping
+    @PostMapping("create")
     public String processCreateEventCategoryForm(@Valid @ModelAttribute EventCategory eventCategory, Errors errors, Model model) {
         if (errors.hasErrors()) {
             model.addAttribute("title", "Create Category");
@@ -40,6 +40,6 @@ public class EventCategoryController {
             return "eventCategories/create";
         }
         eventCategoryRepository.save(eventCategory);
-        return "redirect:";
+        return "redirect:/eventCategories";
     }
 }
